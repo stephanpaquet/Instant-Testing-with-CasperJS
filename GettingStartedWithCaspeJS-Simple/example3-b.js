78648d659be0906a48feaca0caab74f035c3c28a
spaquet <stephan.paquet@duproprio.com>
2015-07-21 11:02:41 -0400

    Added example 3

---
+++ GettingStartedWithCaspeJS-Simple/example3-b.js
@@ -0,0 +1,24 @@
+/*global casper */
+
+// casperjs test example3-a.js
+
+// We need to set up a regular user agent to make sure that geonames.org will accept to process our request.
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
+
+casper.test.begin('Search a city by name', 1, function (test) {
+    "use strict";
+
+    casper.start('http://localhost:8000/example3.html', function () {
+        this.sendKeys("input#searchedlocation", "barcelona");
+        this.click("button#search");
+    });
+
+    casper.waitForSelector('div.success', function () {
+        test.assertTextExists('Barcelona (Catalonia)', 'Barcelona (Catalonia) has been found.');
+    });
+
+    casper.run(function () {
+        test.done();
+    });
+});
+    
